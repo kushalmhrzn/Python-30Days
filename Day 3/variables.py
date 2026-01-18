b8e7ea4572e18b829bcd9218d3fdf06ef96a1e2d
Your Name <your@example.co>
2026-01-18 08:19:42 +0545

    day 3

--- Day 3/variables.py
+++ Day 3/variables.py
@@ -1,5 +1,47 @@
 #assign value to variable name
 
-name = 'kushal'
+#print(name)
 
-print(name)
+#assiging new value to the name 
+#name = 'maharjan'
+#print(name)
+
+#using multiple variables at a same time
+#umbers = [10, 20, 30, 40]
+#names = ["Alice", "Bob", "Charlie"]
+#print(numbers)
+#print(names)
+
+#accessing values from the list using index 
+#numbers = [10, 20, 30]
+#print(numbers[0])  # prints 10
+#print(numbers[1])  # prints 20
+
+#2 * 3.1416 * 10
+
+#pi = 3.1416
+#radius = 10
+
+#2 * pi * radius #(we can use variables instead of hardcoding values)
+
+#looping variables
+
+animal_name= {
+    'dog',
+    'cat',
+    'elephant',
+    'tiger',
+    'lion',
+}
+for animal in animal_name:  #print() automatically adds a line break after each call
+  print(animal)             #The loop calls print() multiple times (once per item)
+                            #Without the loop, print() is called only once
+    
+
+counter = 10
+while counter:
+    print(counter)
+    counter += 1 > 15
+
+
+ 
