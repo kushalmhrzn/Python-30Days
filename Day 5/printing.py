9c06e47c981092d85e1f563a3dc8db682ced4047
Your Name <your@example.co>
2026-01-21 21:28:00 +0545

    day 5

---
+++ Day 5/printing.py
@@ -0,0 +1,28 @@
+#here we will learn about the printing methods
+
+#basic printing
+print('Good morning')
+print("its raining today")
+
+#python print() with end parameter
+#end parameter is used to print something in a same line from starting of end to the next print
+print('hey!!', end='')
+
+print('whats up', end='')
+
+print('hope your rocking')
+
+#python print() with sep parameter
+#sep parameter is used to separate multiple values inside print function
+print('hello', 'world', 'welcome', 'to', 'python', sep='#') #inside sep we can give any separator
+
+#print variable
+
+number = 10.6
+name = "kushal" #we need to keep string inside quotes
+
+print('the number is:', number)
+print('the name is:', name)
+
+#we can join concatenate strings using +
+print('My name is' + ' ram')
---
+++ Day 5/taking_input.py
@@ -0,0 +1,16 @@
+#using input() to take user input
+
+#name = input('enter your name:')
+
+#print('You Entered name is:', name)
+
+
+#asking for two numbers
+
+
+first_number =float(input('enter the first number:'))
+second_number = int(input('enter the second number:'))
+
+sum = first_number + second_number #python uses dynamic typing so we don't need to declare sum.
+
+print('the sum of two numbers is:', sum)  #this will concatenate the two inputs as strings
