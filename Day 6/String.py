9b71a27061b74254fea7f62e4b1158d4776a9da2
Your Name <your@example.co>
2026-02-04 17:11:38 +0545

    day 6

---
+++ Day 6/String.py
@@ -0,0 +1,23 @@
+print("this is 'kushal'")  #quotes inside quotes
+
+#assing string to a variable
+a = 'ram'
+print(a)
+
+#multiline string
+#it helps to seperate the lines
+b = """this is the multiline string
+which we can write 
+in this way"""
+print(b)
+
+#string array
+
+k = "this is kushal maharjan" #this line is string array
+print(k[1]) #it will print the seletect number index character
+print(k[2])
+print(k[3])
+print(k[4])
+print(k[5])
+print(k[6])
+print(k[7])
